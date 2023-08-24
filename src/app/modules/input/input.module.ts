47e2388e3143dd54a695bdc1ee33476dfe494bca
Zix-Dev <Zix.Developmen@gmail.com>
2023-08-24 19:16:28 +0200

    fix

--- src/app/modules/input/input.module.ts
+++ src/app/modules/input/input.module.ts
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
     Components.TextAreaComponent,
   ],
   exports: [
+    FormsModule,
     Components.BtnComponent,
     Components.TextBoxComponent,
     Components.NumberBoxComponent,
